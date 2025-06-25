353ebb415db3fbcc0d2595403239c64af807f6f7
abrar-balti <80289777+abrar-balti@users.noreply.github.com>
2025-06-25 19:39:00 +0500

    Initial commit

---
+++ Model/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace StudentAPI.Model
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public int Age { get; set; }
+        public required string Email { get; set; }
+    }
+}
